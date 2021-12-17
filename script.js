c74981085e77221321722dfa5013fc0bc3d30dcf
Jose Christopher Toscano <jctoscano1@gmail.com>
2021-12-17 11:59:45 -0800

    User can now toggle the read option and will update the data from the table

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-let myLibrary = [{author:'a',title:'a',pages:1,read:true}];
+let myLibrary = [{author:'a',title:'a',pages:1,read:true},
+{author:'a',title:'a',pages:1,read:false}];
 const container = document.querySelector('.container');
 const addBtn = document.querySelector('.icon-box');
 const form = document.querySelector('.form');
@@ -7,6 +8,12 @@ const authorInput = document.querySelector('#author');
 const titleInput = document.querySelector('#title');
 const pagesInput = document.querySelector('#pages');
 const readInput = document.querySelector('#read');
+const checkMarkIcon = `<svg xmlns="http://www.w3.org/2000/svg" class="icon--read" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" />
+</svg>`;
+const minusIcon = `<svg xmlns="http://www.w3.org/2000/svg" class="icon--read" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M18 12H6" />
+</svg>`;
 
 function Book(title,author,pages,read) {
     this.title = title;
@@ -26,17 +33,13 @@ function addBookToLibrary() {
 
 function getBooksHTML() {
     return myLibrary.reduce((htmlStr,book,index) => {
-        const bookHTML = `<tr>
+        const bookHTML = `<tr data-index=${index}>
         <td class="data number">${index + 1}</td>
         <td class="data">${book.author}</td>
         <td class="data">${book.title}</td>
         <td class="data">${book.pages}</td>
-        ${book.read ? `<td class="data read">
-        <svg xmlns="http://www.w3.org/2000/svg" class="icon--read" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" />
-        </svg></td>` : `<td class="data not-read"><svg xmlns="http://www.w3.org/2000/svg" class="icon--read" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M18 12H6" />
-        </svg></td>`}
+        ${book.read ? `<td class="data read did-read">
+        ${checkMarkIcon}</td>` : `<td class="data read not-read">${minusIcon}</td>`}
         <td class="data"><div class="option"><svg xmlns="http://www.w3.org/2000/svg" class="icon" fill="none" viewBox="0 0 24 24" stroke="currentColor">
         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
       </svg><div></td>
@@ -103,6 +106,22 @@ document.body.addEventListener('click', (e) => {
     if(!isFormSelected) {
         switchToTable();
     }
+});
+
+container.addEventListener('click', (e) => {
+    e.stopPropagation();
+    const readContainer = e.target.closest('.read');
+    if(!readContainer) return;
+    const rowIndex = e.target.closest('tr').dataset.index;
+    myLibrary[rowIndex].read = !myLibrary[rowIndex].read;
+    if(readContainer.classList.contains('did-read')) {
+        readContainer.innerHTML = minusIcon;
+    } else {
+        readContainer.innerHTML = checkMarkIcon;
+    }
+    readContainer.classList.toggle('did-read');
+    readContainer.classList.toggle('not-read');
+
 });
 
 addBtn.addEventListener('click',switchToForm);
