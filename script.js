1d2525dec7709bb49e380ea177df14729584fbd4
Jose Christopher Toscano <jctoscano1@gmail.com>
2021-12-17 00:17:49 -0800

    table is built through javascript instead of being hardcoded

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-let myLibrary = [{title:"Green Eggs and Ham", author:"Dr.Seuss",pages:32},
+let myLibrary = [];/*[{title:"Green Eggs and Ham", author:"Dr.Seuss",pages:32},
 {title:"Percy Jackson", author:"Rick Riordan",pages:269},];
+const container = document.querySelector('.container');*/
 const container = document.querySelector('.container');
 
 function Book(title,author,pages) {
@@ -25,4 +26,55 @@ function displayBooks() {
     }); 
 }
 
+function buildTable() {
+    let tableBodyHTML = ``;
+    if(myLibrary.length === 0) {
+        tableBodyHTML = `<tr>
+        <td colspan="4" class="message--empty">Nothing to display :(</td>
+    </tr>;`
+    }
+    const tableHTML = `<table class="table">
+    <thead class="table-head">
+        <tr>
+            <th class="data-header">Title</th>
+            <th class="data-header">Author</th>
+            <th class="data-header">Pages</th>
+            <th class="data-header">Read?</th>
+        </tr>
+    </thead>
+    <tbody>
+    ${tableBodyHTML}
+    </tbody>
+    </table>`;
+    container.innerHTML = tableHTML + `<form class="form" action="" method=""> 
+    <h2 class="form-title">Add New Book</h2>
+    <div class="form-field">
+        <label for="author">Author</label>
+        <input class="input" id="author" type="text" placeholder="author name">
+    </div> 
+    <div class="form-field">
+        <label for="title">Title</label>
+        <input class="input" id="title" type="text" placeholder="book title">
+    </div>
+    <div class="form-field">
+        <label for="pages">Pages</label>
+        <input class="input" id="pages" type="number" placeholder="enter total pages">
+    </div>
+    <div class="form-field">
+        <label for="read">Read?</label>
+        <select class="dropdown" id="read">
+            <option value="Yes">Yes</option>
+            <option value="No">No</option>
+        </select>
+    </div>
+    <button class="btn">Submit</button>
+</form>
+<div class="icon-box">
+    <svg xmlns="http://www.w3.org/2000/svg" class="icon--add" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4v16m8-8H4" />
+    </svg>
+</div>`;
+}
+
 displayBooks();
+buildTable();
