9d62afdf45300d6a60b4a117deeb1e64f1725b50
Jose Christopher Toscano <jctoscano1@gmail.com>
2021-12-18 09:34:00 -0800

    Created delete book message

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let myLibrary = [{author:'a',title:'a',pages:1,read:true},
+let myLibrary = [{author:'author',title:'a title',pages:1,read:true},
 {author:'a',title:'a',pages:1,read:false}];
 const container = document.querySelector('.container');
 const addBtn = document.querySelector('.icon-box');
@@ -35,15 +35,15 @@ function getBooksHTML() {
     return myLibrary.reduce((htmlStr,book,index) => {
         const bookHTML = `<tr data-index=${index}>
         <td class="data number">${index + 1}</td>
-        <td class="data">${book.author}</td>
         <td class="data">${book.title}</td>
+        <td class="data">${book.author}</td>
         <td class="data">${book.pages}</td>
         ${book.read ? `<td class="data read did-read">
         ${checkMarkIcon}</td>` : `<td class="data read not-read">${minusIcon}</td>`}
-        <td class="data"><div class="option"><svg xmlns="http://www.w3.org/2000/svg" class="icon" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+        <td class="data"><div class="option"><svg xmlns="http://www.w3.org/2000/svg" class="icon delete" fill="none" viewBox="0 0 24 24" stroke="currentColor">
         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
       </svg><div></td>
-        <td class="data"><div class="option"><svg xmlns="http://www.w3.org/2000/svg" class="icon" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+        <td class="data"><div class="option"><svg xmlns="http://www.w3.org/2000/svg" class="icon edit" fill="none" viewBox="0 0 24 24" stroke="currentColor">
         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z" />
       </svg></div></td>
         </tr>`;
@@ -64,8 +64,8 @@ function buildTable() {
     <thead class="table-head">
         <tr>
             <th class="data-header">#</th>
-            <th class="data-header">Author</th>
             <th class="data-header">Title</th>
+            <th class="data-header">Author</th>
             <th class="data-header">Pages</th>
             <th class="data-header">Read?</th>
             <th class="data-header">Delete</th>
@@ -97,7 +97,29 @@ function switchToForm(e) {
     container.style.height = "0vh";
 }
 
-buildTable();
+function toggleRead(e) {
+    const readContainer = e.target.closest('.read');
+    if(!readContainer) return;
+    const rowIndex = e.target.closest('tr').dataset.index;
+    myLibrary[rowIndex].read = !myLibrary[rowIndex].read;
+    if(readContainer.classList.contains('did-read')) {
+        readContainer.innerHTML = minusIcon;
+    } else {
+        readContainer.innerHTML = checkMarkIcon;
+    }
+    readContainer.classList.toggle('did-read');
+    readContainer.classList.toggle('not-read');
+}
+
+function deleteBook(e) {
+    const deleteContainer = e.target.closest('.delete');
+    if(!deleteContainer) return;
+    addBtn.style.display='none';
+    const rowIndex = e.target.closest('tr').dataset.index;
+    tableContainer.style.display="none";
+}
+
+// buildTable();
 
 document.body.addEventListener('click', (e) => {
     const form = document.querySelector('.form');
@@ -110,18 +132,8 @@ document.body.addEventListener('click', (e) => {
 
 container.addEventListener('click', (e) => {
     e.stopPropagation();
-    const readContainer = e.target.closest('.read');
-    if(!readContainer) return;
-    const rowIndex = e.target.closest('tr').dataset.index;
-    myLibrary[rowIndex].read = !myLibrary[rowIndex].read;
-    if(readContainer.classList.contains('did-read')) {
-        readContainer.innerHTML = minusIcon;
-    } else {
-        readContainer.innerHTML = checkMarkIcon;
-    }
-    readContainer.classList.toggle('did-read');
-    readContainer.classList.toggle('not-read');
-
+    toggleRead(e);
+    deleteBook(e);
 });
 
 addBtn.addEventListener('click',switchToForm);
